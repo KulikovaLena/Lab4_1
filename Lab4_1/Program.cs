4f0a5bf897398f703d1d72f03c033afcd0c11ef7
11111 <11111@DESKTOP-I2P1BG6>
2021-11-20 00:07:37 +0300

    Add project files.

---
+++ Lab4_1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            int sum = 0;
+            Console.WriteLine("Vvedite znachenie");
+            n = Convert.ToInt32(Console.ReadLine());
+            for (int x = 1; x < 2 * n; x += 2)
+            {
+                sum += x;
+                Console.WriteLine("kvadrat raven {0}", sum);
+            }
+            Console.ReadKey();
+        }
+    }
+}
